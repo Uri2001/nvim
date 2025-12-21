635d28a05db291f9dde6e1a7bd4703a03f0eae4e
y.gavrilenko <y.gavrilenko@rt-solar.ru>
2025-12-21 20:45:09 +0300

    Dec 2025

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require('utils.lualine_debug')
+require("utils.lualine_debug")
+require("blink.cmp").setup({
+  keymap = {
+    preset = "super-tab",
+  },
+})
