d19db01ecf284969cf728371f7494db2af03c41f
Uri2001 <uri_2001@mail.ru>
2026-01-04 17:51:22 +0300

    Fix lsp error

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -5,4 +5,13 @@ return {
 
   -- добавляем полезные плагины
   { "RRethy/vim-illuminate", event = "VeryLazy" },
+
+  -- Work around systemd_ls recursion during auto-enable
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.handlers = opts.handlers or {}
+      opts.handlers["systemd_ls"] = function() end
+    end,
+  },
 }
