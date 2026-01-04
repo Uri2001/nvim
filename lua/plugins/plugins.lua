e9fe5146f8b3c1e9ec4fd07e5b88b77a2620fad8
y.gavrilenko <y.gavrilenko@rt-solar.ru>
2026-01-04 15:55:04 +0300

    fix plugins duplicates

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,8 +1,8 @@
 return {
   -- disable mini.starter (используем alpha)
   { "nvim-mini/mini.starter", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
 
   -- добавляем полезные плагины
-  { "kylechui/nvim-surround", event = "VeryLazy", opts = {} },
   { "RRethy/vim-illuminate", event = "VeryLazy" },
 }
